a14fd9c0a30d4c5309277e7a36b604b9368ff526
Jason Ross <jasnross@gmail.com>
2026-04-03 09:53:16 -0400

    fix(agentspec): remove redundant version line and hardcode URLs

--- Formula/agentspec.rb
+++ Formula/agentspec.rb
@@ -1,23 +1,21 @@
 class Agentspec < Formula
-  # Replace sha256 values from the corresponding release SHA256SUMS.
   desc "Compile provider-neutral agent and skill specs"
   homepage "https://github.com/jasnross/agentspec"
   license "MIT"
-  version "0.1.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/jasnross/agentspec/releases/download/v#{version}/agentspec-v#{version}-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/jasnross/agentspec/releases/download/v0.1.0/agentspec-v0.1.0-aarch64-apple-darwin.tar.gz"
       sha256 "ba67aba1e5258e558e2f5e4c438a905f38975cea2f6cc173e07a310bd4360889"
     else
-      url "https://github.com/jasnross/agentspec/releases/download/v#{version}/agentspec-v#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/jasnross/agentspec/releases/download/v0.1.0/agentspec-v0.1.0-x86_64-apple-darwin.tar.gz"
       sha256 "6c2dd6991c262c2899fe1b786993f97ca00af8a9c8ca15020b8090ced4193be0"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/jasnross/agentspec/releases/download/v#{version}/agentspec-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/jasnross/agentspec/releases/download/v0.1.0/agentspec-v0.1.0-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "d3941d9d929b4c7ec6690231bf0e4abbd68ac340c2d0142085ece5e790ebc12b"
     else
       odie "agentspec binaries are currently published only for x86_64 Linux"
